45b0c6c55f91ba28badd919e00eac328cc7f3237
dedeleledede <erikzavan@gmail.com>
2026-04-26 22:12:54 -0300

    add health page

---
+++ backend/src/main/java/com/zavan/dedesite/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.zavan.dedesite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "health";
+    }
+}
